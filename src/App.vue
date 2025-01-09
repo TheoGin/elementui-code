f4ef85fd08f03db17714d9f9ec91ca7a45296718
Sophotheo <fuchaojun@readboy.com>
2025-01-09 08:30:56 +0800

    feat: 混合布局测试

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,22 @@
       <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
       <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
     </el-row>
+    <h1>混合布局</h1>
+    <el-row :gutter="20">
+      <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
   </div>
 </template>
 
